2a98900fe917f53d69bbc483cee6011cc7ede7b6
turbs <walker.turbyfill@gmail.com>
2024-11-26 00:07:32 -0500

    changed a and d directions

--- player.py
+++ player.py
@@ -28,9 +28,9 @@ class Player(CircleShape):
         
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.rotate(dt)
         if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_s]:
             self.rotate(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
